462887027a0a0305d2b7f0ad905947ad88d3304f
martin-samson-png <martinsams64@gmail.com>
2025-05-22 12:02:48 +0200

    feat : ajout du html

---
+++ script.js
@@ -0,0 +1,7 @@
+//création du DOM
+const body = document.body;
+const mainEventDiv = document.createElement("div");
+const eventTitle = document.createElement("h3");
+const eventDateDiv = document.createElement("div");
+const eventPlaceDiv = document.createElement("div");
+con;
