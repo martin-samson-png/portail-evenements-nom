5af1a414adaefbb50a8efcdab57825c4f689085d
martin-samson-png <martinsams64@gmail.com>
2025-05-22 13:31:24 +0200

    feat : ajout de la template et du clonage

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 //création du DOM
-const body = document.body;
-const mainEventDiv = document.createElement("div");
-const eventTitle = document.createElement("h3");
-const eventDateDiv = document.createElement("div");
-const eventPlaceDiv = document.createElement("div");
-con;
+const planningList = document.getElementById("planningList");
+const template = document.getElementById("eventTemplate");
+
+const fetchData = fetch(
+  "https://demo.theeventscalendar.com/wp-json/tribe/events/v1/events"
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Erreur réseau");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data.events);
+
+    data.events.forEach((event, index) => {
+      //récuperation des données
+      const titleEvent = event.title;
+      const descriptionEvent = event.description;
+      const dateEvent = event.start_date;
+      let placeEvent;
+      if (event.venue && event.venue.address) {
+        placeEvent = `Adresse ${event.venue.address}`;
+      } else {
+        placeEvent = `Pas d'adresse disponible`;
+      }
+      const urlEvent = event.url;
+
+      //clonage des données
+      const clone = template.content.cloneNode(true);
+      clone.querySelector(".titleTemplate").textContent = titleEvent;
+      clone.querySelector(".dateTemplate").textContent = dateEvent;
+      clone.querySelector(".placeTemplate").textContent = placeEvent;
+      planningList.appendChild(clone);
+    });
+  })
+  .catch((error) => {
+    console.error("Erreur :", error);
+  });
+//eventTitle.textContent = key.title;
